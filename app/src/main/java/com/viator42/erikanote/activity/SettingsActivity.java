c8691899703132b24e04f1cc506b620f6ce12fec
Chuanqing.Wu <via313@gmail.com>
2016-08-09 16:58:30 +0800

    20160809

--- app/src/main/java/com/viator42/erikanote/activity/SettingsActivity.java
+++ app/src/main/java/com/viator42/erikanote/activity/SettingsActivity.java
@@ -1,8 +1,8 @@
 package com.viator42.erikanote.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -22,8 +22,10 @@ public class SettingsActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                Intent intent = new Intent(SettingsActivity.this, DevActivity.class);
+                startActivity(intent);
             }
         });
     }
