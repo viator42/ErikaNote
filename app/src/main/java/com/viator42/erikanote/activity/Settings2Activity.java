8c7edd8e4c6f3737f10873d354a5f765ec697afb
Viator42 <via313@gmail.com>
2016-08-16 00:01:01 +0800

    pref activity

---
+++ app/src/main/java/com/viator42/erikanote/activity/Settings2Activity.java
@@ -0,0 +1,32 @@
+package com.viator42.erikanote.activity;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+import com.viator42.erikanote.R;
+
+public class Settings2Activity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings2);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//                Intent intent = new Intent(SettingsActivity.this, DevActivity.class);
+//                startActivity(intent);
+//
+//                //设定余额 名字 使用refActivity
+//
+//            }
+//        });
+    }
+
+}
